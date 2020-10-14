8cf0c8442e89d164eba0b293882d7470be680875
Jesse-Wurm <Slz00itt@gmail.com>
2020-10-14 13:59:06 -0800

    completed the program

--- first/first.py
+++ first/first.py
@@ -1,1 +1,12 @@
-l
+# defining the variable "cars" as an array
+cars = ["Ford", "Volvo", "BMW", "Mercedes", "Toyota", "Chevrolet"]
+
+
+def first(array, n):
+    # declaring "n" as the first element in the array "cars"
+    x = cars[n]
+    # returning the value of "n"
+    print(x)
+
+
+first(cars, 0)
