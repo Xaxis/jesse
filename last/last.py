a400fabdd6230e8f58805cb3f140d620dc7de6d5
Jesse-Wurm@github.com <Slz00itt@gmail.com>
2020-10-14 19:09:00 -0800

    completed 2nd array problem

---
+++ last/last.py
@@ -0,0 +1,11 @@
+cars = ["Ford", "Volvo", "BMW", "Mercedes", "Toyota", "Chevrolet"]
+
+def last(arr, n=0):
+    if n is last.__defaults__[0]:
+        return arr[-1]
+    else:
+        return arr[-n:]
+
+print(last(cars))
+print(last(cars, 2))
+
