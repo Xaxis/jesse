5b0286937fcf5625f51464c54346123713579ea6
Jesse-Wurm@github.com <Slz00itt@gmail.com>
2020-10-15 13:29:30 -0800

    got it to work with 2 arguements. Unable to call more than 3 arguements

--- without/without.py
+++ without/without.py
@@ -1,12 +1,12 @@
 nums = [1, 2, 1, 0, 3, 1, 4]
-
+# the function 'without' wont allow for multiple arugments
 def without(arr, n=0):
 
     if n is without.__defaults__[0]:
         return arr[n:]
     else:
-        for i in range(len(arr)):
+        while n in arr:
             arr.remove(n)
-            return arr
+        return arr
 print(without(nums))
 print(without(nums, 1))
