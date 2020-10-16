0f8658835b41b90fd66d05ea53ad4905d5e6d23d
Jesse-Wurm@github.com <Slz00itt@gmail.com>
2020-10-16 10:58:13 -0800

    completed task

--- without/without.py
+++ without/without.py
@@ -1,12 +1,8 @@
 nums = [1, 2, 1, 0, 3, 1, 4]
 # the function 'without' wont allow for multiple arugments
-def without(arr, n=0):
+def without(arr1, arr2=[]):
+    diff = list(set(arr1) - set(arr2))
+    return diff
 
-    if n is without.__defaults__[0]:
-        return arr[n:]
-    else:
-        while n in arr:
-            arr.remove(n)
-        return arr
-print(without(nums))
-print(without(nums, 1))
+#print(without(nums))
+print(without(nums, [1,2,3]))
