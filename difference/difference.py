6ef84eaff7dc7ea6cf97056cf4d8f1b421f43f22
Jesse-Wurm@github.com <Slz00itt@gmail.com>
2020-10-16 11:48:55 -0800

    completed challenge

--- difference/difference.py
+++ difference/difference.py
@@ -1,4 +1,10 @@
-def difference(arr1, arr2):
-    pass
+def difference(arr1=[], arr2=[]):
+    findUnique = list(set(arr1) - set(arr2))
+    return findUnique
 
-#print(difference([1,2,3,4,5], [2,5,10]))
+print(difference([1,2,3,4,5], [2,5,10]))
+
+
+# def without(arr1, arr2=[]):
+#     subtract = list(set(arr1) - set(arr2))
+#     return subtract
