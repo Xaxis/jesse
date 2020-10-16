156e23f459387d5e80c6cc164ffce54e756d2d94
Jesse-Wurm@github.com <Slz00itt@gmail.com>
2020-10-16 12:38:27 -0800

    reverted change on difference.py; accidently uploaded testing code

--- array/difference/difference.py
+++ array/difference/difference.py
@@ -2,4 +2,5 @@ def difference(arr1=[], arr2=[]):
     findUnique = list(set(arr1) - set(arr2))
     return findUnique
 
-print(difference([1,[1,2,3],3,4,5,6,6], [[1,2,3unb],5,4,6]))
+
+print(difference([1, 2, 3, 4, 5], [5, 2, 10]))
