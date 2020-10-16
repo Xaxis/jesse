b186f11502ead95d23a02da47a8fba5e17ff5663
Jesse-Wurm@github.com <Slz00itt@gmail.com>
2020-10-16 12:33:33 -0800

    moved all array functions to the array folder

--- array/difference/difference.py
+++ array/difference/difference.py
@@ -2,4 +2,4 @@ def difference(arr1=[], arr2=[]):
     findUnique = list(set(arr1) - set(arr2))
     return findUnique
 
-print(difference([1,2,3,4,5], [2,5,10]))
+print(difference([1,[1,2,3],3,4,5,6,6], [[1,2,3unb],5,4,6]))
