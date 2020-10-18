9da4fef6153900228f403a70d5faec97bbbdfe92
Jesse-Wurm@github.com <Slz00itt@gmail.com>
2020-10-17 19:48:59 -0800

    pushing current progress

--- array/union/union.py
+++ array/union/union.py
@@ -7,13 +7,22 @@
 
 #test
 
-def union(*manyarrs):
-    # print(len(manyarrs))
-    # print(type(manyarrs))
-    one = set(manyarrs[0])
-    two = set(manyarrs[1])
-    three = set(manyarrs[2])
-    unique_items = list((one ^ two) ^ (three))
-    return unique_items
+# def union(*manyarrs):
+#     # print(len(manyarrs))
+#     # print(type(manyarrs))
+#     one = set(manyarrs[0])
+#     two = set(manyarrs[1])
+#     three = set(manyarrs[2])
+#     unique_items = list((one ^ two) ^ (three))
+#     return unique_items
+#
+# print(union([1, 2, 3], [101, 2, 1, 10], [2, 1]))
+
+# for loop to iterate over the tuple
+
+def union(*foo):
+    for i in enumerate(foo):
+        print(i)
+    return
 
 print(union([1, 2, 3], [101, 2, 1, 10], [2, 1]))
